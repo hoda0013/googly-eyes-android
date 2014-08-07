89782fc3bd0b0165cc73cc1715698f411a02d53c
hoda0013 <dillon@coderow.com>
2014-08-07 11:54:23 -0500

    Implemented eyes so only one can have focus at a time. Clicking actionbar adds eye. Flinging left or right deletes eye with animation and toast.

--- app/src/main/java/com/djh/googlyeyes/GooglyEyeWidget.java
+++ app/src/main/java/com/djh/googlyeyes/GooglyEyeWidget.java
@@ -1,16 +1,21 @@
 package com.djh.googlyeyes;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * Created by dillonhodapp on 7/30/14.
@@ -21,15 +26,37 @@ public class GooglyEyeWidget extends View implements SensorEventListener{
     private Sensor mAccelerometer;
     private long lastTime;
     private static final long THRESHOLD_TIME = 16; //milliseconds
-    private static final float SCLERA_RADIUS = 100;
-    private static final float PUPIL_RADIUS = 80;
+    private static final int HANDLE_WIDTH = 60;
+    private static final int MIN_WIDTH = 24;
 
-
+    private Context mContext;
     private Paint mSclera;
     private Paint mPupil;
-    private Context mContext;
+    private Paint mBoundingHandle;
+    private Paint mBoundingBox;
+    private Rect mBoundingRect = new Rect();
+    private Rect mUpperLeftHandle = new Rect();
+    private Rect mUpperRightHandle = new Rect();
+    private Rect mLowerLeftHandle = new Rect();
+    private Rect mLowerRightHandle = new Rect();
     private float unitX;
     private float unitY;
+
+    private int boxWidth = 100;
+    private int boxCornerX = 100;
+    private int boxCornerY = 100;
+    private Mode mMode;
+
+
+    public enum Mode {
+        DRAGGING,
+        RESIZING_UPPER_LEFT,
+        RESIZING_UPPER_RIGHT,
+        RESIZING_LOWER_LEFT,
+        RESIZING_LOWER_RIGHT,
+        EDITING,
+        PLACED
+    }
 
     public GooglyEyeWidget(Context context) {
         super(context);
@@ -54,15 +81,28 @@ public class GooglyEyeWidget extends View implements SensorEventListener{
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int x = getMeasuredWidth() / 2;
-        int y = getMeasuredHeight() / 2;
-        int scleraRadius = x;
-        int pupilRadius = (scleraRadius * 2000) / 3000;
+        int x = boxCornerX + (boxWidth + 2 * HANDLE_WIDTH) / 2;
+        int y = boxCornerY + (boxWidth + 2 * HANDLE_WIDTH) / 2;
+        int pupilRadius = (boxWidth * 2000) / 6000;
 
+        if (mMode != Mode.PLACED) {
+            //draw bounding box
+            mBoundingRect.set(boxCornerX + HANDLE_WIDTH / 2, boxCornerY + HANDLE_WIDTH / 2, boxCornerX + HANDLE_WIDTH + boxWidth + HANDLE_WIDTH / 2, boxCornerY + HANDLE_WIDTH + boxWidth + HANDLE_WIDTH / 2);
+            canvas.drawRect(mBoundingRect, mBoundingBox);
+            //draw handles
+            mUpperLeftHandle.set(boxCornerX, boxCornerY, boxCornerX + HANDLE_WIDTH, boxCornerY + HANDLE_WIDTH);
+            mUpperRightHandle.set(boxCornerX + boxWidth + HANDLE_WIDTH, boxCornerY, boxCornerX + boxWidth + 2 * HANDLE_WIDTH, boxCornerY + HANDLE_WIDTH);
+            mLowerLeftHandle.set(boxCornerX, boxCornerY + boxWidth + HANDLE_WIDTH, boxCornerX + HANDLE_WIDTH, boxCornerY + boxWidth + 2 * HANDLE_WIDTH);
+            mLowerRightHandle.set(boxCornerX + boxWidth + HANDLE_WIDTH, boxCornerY + boxWidth + HANDLE_WIDTH, boxCornerX + boxWidth + 2 * HANDLE_WIDTH, boxCornerY + boxWidth + 2 * HANDLE_WIDTH);
+            canvas.drawRect(mUpperLeftHandle, mBoundingHandle);
+            canvas.drawRect(mUpperRightHandle, mBoundingHandle);
+            canvas.drawRect(mLowerLeftHandle, mBoundingHandle);
+            canvas.drawRect(mLowerRightHandle, mBoundingHandle);
+        }
 
-        canvas.drawCircle(x, y, scleraRadius, mSclera);
-        canvas.drawCircle(x + (unitX * (scleraRadius - pupilRadius)),  y + (unitY * (scleraRadius - pupilRadius)), pupilRadius, mPupil);
-
+        //draw eyeball
+        canvas.drawCircle(x, y, boxWidth / 2, mSclera);
+        canvas.drawCircle(x + (unitX * (boxWidth / 2 - pupilRadius)),  y + (unitY * (boxWidth / 2 - pupilRadius)), pupilRadius, mPupil);
     }
 
     @Override
@@ -71,17 +111,138 @@ public class GooglyEyeWidget extends View implements SensorEventListener{
 
     }
 
+public boolean isTouchingBoundingBox(float eventX, float eventY) {
+    if (eventX >= boxCornerX && eventX <= (boxCornerX + boxWidth + 2 * HANDLE_WIDTH)) {
+        if (eventY >= boxCornerY && eventY <= (boxCornerY + boxWidth + 2 * HANDLE_WIDTH)) {
+            if (!isTouchingUpperLeft(eventX, eventY) && !isTouchingUpperRight(eventX, eventY) && !isTouchingLowerLeft(eventX, eventY) && !isTouchingLowerRight(eventX,eventY)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+    public boolean isTouchingUpperLeft(float eventX, float eventY) {
+        if (eventX >= boxCornerX && eventX <= (boxCornerX + HANDLE_WIDTH)) {
+            if (eventY >= boxCornerY && eventY <= (boxCornerY + HANDLE_WIDTH)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isTouchingUpperRight(float eventX, float eventY) {
+        if (eventX >= (boxCornerX + boxWidth + HANDLE_WIDTH) && eventX <= (boxCornerX + boxWidth + 2 * HANDLE_WIDTH)) {
+            if (eventY >= boxCornerY && eventY <= (boxCornerY + HANDLE_WIDTH)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isTouchingLowerLeft(float eventX, float eventY) {
+        if (eventX >= boxCornerX && eventX <= (boxCornerX + HANDLE_WIDTH)) {
+            if (eventY >= (boxCornerY + boxWidth + HANDLE_WIDTH) && eventY <= (boxCornerY + boxWidth + 2 * HANDLE_WIDTH)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isTouchingLowerRight(float eventX, float eventY) {
+        if (eventX >= (boxCornerX + boxWidth + HANDLE_WIDTH) && eventX <= (boxCornerX + boxWidth + 2 * HANDLE_WIDTH)) {
+            if (eventY >= (boxCornerY + boxWidth + HANDLE_WIDTH) && eventY <= (boxCornerY + boxWidth + 2 * HANDLE_WIDTH)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public void setMode(Mode mode) {
+        mMode = mode;
+    }
+
+    public Mode getMode() {
+        return mMode;
+    }
+
+    public void setDraggingCoords(int x, int y) {
+        boxCornerX = boxCornerX + x;
+        boxCornerY = boxCornerY + y;
+        invalidate();
+    }
+
+    public void resizeUpperLeft(int delta) {
+        if ((boxWidth - delta) > MIN_WIDTH) {
+            boxCornerX = boxCornerX + delta;
+            boxCornerY = boxCornerY + delta;
+            boxWidth = boxWidth - delta;
+        }
+        invalidate();
+    }
+
+    public void resizeUpperRight(int delta) {
+        if ((boxWidth + delta) > MIN_WIDTH) {
+            boxWidth = boxWidth + delta;
+            boxCornerY = boxCornerY - delta;
+        }
+        invalidate();
+    }
+
+    public void resizeLowerLeft(int delta) {
+
+        if ((boxWidth - delta) > MIN_WIDTH) {
+            boxWidth = boxWidth - delta;
+            boxCornerX = boxCornerX + delta;
+        }
+        invalidate();
+    }
+
+    public void resizeLowerRight(int delta) {
+        if ((boxWidth + delta) > MIN_WIDTH) {
+            boxWidth = boxWidth + delta;
+        }
+        invalidate();
+    }
+
     private void init() {
+        mMode = Mode.EDITING;
         mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         mSclera = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPupil = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBoundingHandle = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBoundingBox = new Paint(Paint.ANTI_ALIAS_FLAG);
+
         mSclera.setColor(getResources().getColor(android.R.color.white));
         mPupil.setColor(getResources().getColor(android.R.color.black));
         mSclera.setStyle(Paint.Style.FILL);
         mPupil.setStyle(Paint.Style.FILL);
+        mBoundingHandle.setColor(getResources().getColor(R.color.bounding_box_handle));
+        mBoundingBox.setColor(getResources().getColor(R.color.bounding_box));
+        mBoundingHandle.setStyle(Paint.Style.FILL);
+        mBoundingBox.setStyle(Paint.Style.STROKE);
+
+
         invalidate();
     }
 
@@ -102,8 +263,6 @@ public class GooglyEyeWidget extends View implements SensorEventListener{
                 x = x / magnitude;
                 y  = y / magnitude;
                 //scale vectors
-//                unitX = (float) (-x * (SCLERA_RADIUS - PUPIL_RADIUS + 1));
-//                unitY = (float) (y * (SCLERA_RADIUS - PUPIL_RADIUS + 1));
 
                 unitX = (float) -x;
                 unitY = (float) y;
@@ -120,5 +279,19 @@ public class GooglyEyeWidget extends View implements SensorEventListener{
 
     }
 
+    public void flingOffScreen() {
+        final int distance = 1000;
+        ValueAnimator animator = ValueAnimator.ofFloat(0,1);
+        animator.setDuration(300);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = ((Float) (animation.getAnimatedValue())).floatValue();
+                boxCornerX = boxCornerX + (int)(value * distance);
+                invalidate();
+            }
+        });
+        animator.start();
+    }
 
 }
--- app/src/main/java/com/djh/googlyeyes/MainActivity.java
+++ app/src/main/java/com/djh/googlyeyes/MainActivity.java
@@ -2,45 +2,167 @@ package com.djh.googlyeyes;
 
 import android.app.Activity;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewManager;
 import android.widget.AbsoluteLayout;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity implements View.OnClickListener{
 
-    AbsoluteLayout mContainer;
+    RelativeLayout mContainer;
+    private Context mContext;
+    private int eyeCounter = 0;
+    private GooglyEyeWidget theEye = null;
+
+    private List<GooglyEyeWidget> listGooglyEyes = new ArrayList<GooglyEyeWidget>();
+    private static final int SWIPE_MIN_DISTANCE = 90;
+    private static final int SWIPE_MAX_OFF_PATH = 100;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 2000;
+    private GestureDetector gestureDetector;
+
+    View.OnTouchListener gestureListener = new View.OnTouchListener() {
+        public boolean onTouch(View v, MotionEvent event) {
+
+            float eventX = event.getX();
+            float eventY = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    tempX = eventX;
+                    tempY = eventY;
+
+                    Log.e("XTOUCH", String.valueOf(eventX));
+                    Log.e("YTOUCH", String.valueOf(eventY));
+
+                    for (GooglyEyeWidget eye : listGooglyEyes) {
+                        if (eye.isTouchingUpperLeft(eventX, eventY)) {
+                            theEye = eye;
+                            theEye.setMode(GooglyEyeWidget.Mode.RESIZING_UPPER_LEFT);
+                            clearOtherEyes(theEye.getId());
+                        } else if (eye.isTouchingUpperRight(eventX, eventY)) {
+                            theEye = eye;
+                            theEye.setMode(GooglyEyeWidget.Mode.RESIZING_UPPER_RIGHT);
+                            clearOtherEyes(theEye.getId());
+                        } else if(eye.isTouchingLowerLeft(eventX, eventY)) {
+                            theEye = eye;
+                            theEye.setMode(GooglyEyeWidget.Mode.RESIZING_LOWER_LEFT);
+                            clearOtherEyes(theEye.getId());
+                        } else if(eye.isTouchingLowerRight(eventX, eventY)) {
+                            theEye = eye;
+                            theEye.setMode(GooglyEyeWidget.Mode.RESIZING_LOWER_RIGHT);
+                            clearOtherEyes(theEye.getId());
+                        } else if(eye.isTouchingBoundingBox(eventX, eventY)){
+                            theEye = eye;
+                            theEye.setMode(GooglyEyeWidget.Mode.DRAGGING);
+                            clearOtherEyes(theEye.getId());
+                        } else {
+                            //touch event did not hit the eye
+                            eye.setMode(GooglyEyeWidget.Mode.PLACED);
+                            if (theEye != null) {
+                                if (eye.getId() == theEye.getId()) {
+                                    theEye = null;
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    if (theEye != null && theEye.getMode() == GooglyEyeWidget.Mode.DRAGGING) {
+                        int deltaX = (int) (eventX - tempX);
+                        int deltaY = (int) (eventY - tempY);
+
+                        theEye.setDraggingCoords(deltaX, deltaY);
+
+                        tempX = eventX;
+                        tempY = eventY;
+                    } else if (theEye != null && theEye.getMode() == GooglyEyeWidget.Mode.RESIZING_UPPER_LEFT) {
+                        int deltaX = (int) (eventX - tempX);
+                        int deltaY = (int) (eventY - tempY);
+                        int delta = 0;
+
+                        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                            delta = deltaY;
+                        } else {
+                            delta = deltaX;
+                        }
+
+                        theEye.resizeUpperLeft(delta);
+                        tempX = eventX;
+                        tempY = eventY;
+                    } else if (theEye != null && theEye.getMode() == GooglyEyeWidget.Mode.RESIZING_UPPER_RIGHT) {
+                        int deltaX = (int) (eventX - tempX);
+                        int deltaY = (int) (eventY + tempY);
+                        int delta = 0;
+
+                        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                            delta = deltaY;
+                        } else {
+                            delta = deltaX;
+                        }
+
+                        theEye.resizeUpperRight(delta);
+                        tempX = eventX;
+                        tempY = eventY;
+                    } else if (theEye != null && theEye.getMode() == GooglyEyeWidget.Mode.RESIZING_LOWER_LEFT) {
+                        int deltaX = (int) (eventX - tempX);
+                        int deltaY = (int) (eventY + tempY);
+                        int delta = 0;
+
+                        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                            delta = deltaY;
+                        } else {
+                            delta = deltaX;
+                        }
+                        theEye.resizeLowerLeft(delta);
+                        tempX = eventX;
+                        tempY = eventY;
+                    } else if (theEye != null && theEye.getMode() == GooglyEyeWidget.Mode.RESIZING_LOWER_RIGHT) {
+                        int deltaX = (int) (eventX - tempX);
+                        int deltaY = (int) (eventY - tempY);
+                        int delta = 0;
+
+                        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                            delta = deltaY;
+                        } else {
+                            delta = deltaX;
+                        }
+                        theEye.resizeLowerRight(delta);
+                        tempX = eventX;
+                        tempY = eventY;
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (theEye != null) {
+                        theEye.setMode(GooglyEyeWidget.Mode.EDITING);
+                    }
+                    break;
+            }
+            return gestureDetector.onTouchEvent(event);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContainer = (AbsoluteLayout) findViewById(R.id.container);
-
-//        xVal = (TextView) findViewById(R.id.xVal);
-//        yVal = (TextView) findViewById(R.id.yVal);
-//        zVal = (TextView) findViewById(R.id.zVal);
-
-
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-//        mSensorManager.unregisterListener(this);
+        mContainer = (RelativeLayout) findViewById(R.id.container);
+        mContext = this;
+        gestureDetector = new GestureDetector(this, new MyGestureDetector());
     }
 
     @Override
@@ -57,11 +179,71 @@ public class MainActivity extends Activity{
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.plus) {
-//            mContainer.addView(new GooglyEyeWidget(this));
+            //If there's an eye in focus, change it to PLACED
+            if (theEye != null) {
+                theEye.setMode(GooglyEyeWidget.Mode.PLACED);
+            }
+            //create new eye and set listeners
+            theEye = new GooglyEyeWidget(this);
+            theEye.setId(eyeCounter++);
+            theEye.setOnClickListener(this);
+            theEye.setOnTouchListener(gestureListener);
+            listGooglyEyes.add(theEye);
+            mContainer.addView(theEye);
+            for (int i = 0; i < listGooglyEyes.size() - 1; i++) {
+                listGooglyEyes.get(i).setMode(GooglyEyeWidget.Mode.PLACED);
+            }
+            theEye.setMode(GooglyEyeWidget.Mode.EDITING);
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    float tempX = 0.0f;
+    float tempY = 0.0f;
 
+    @Override
+    public void onClick(View v) {
+    }
+
+    class MyGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(mContext, getString(R.string.eye_deleted), Toast.LENGTH_SHORT).show();
+                    theEye.flingOffScreen();
+//                    mContainer.removeView(theEye);
+//                    listGooglyEyes.remove(theEye);
+
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Toast.makeText(mContext, getString(R.string.eye_deleted), Toast.LENGTH_SHORT).show();
+                    theEye.flingOffScreen();
+//                    mContainer.removeView(theEye);
+//                    listGooglyEyes.remove(theEye);
+                }
+
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+
+            return true;
+        }
+    }
+
+    private void clearOtherEyes(int id) {
+        for (GooglyEyeWidget eye : listGooglyEyes) {
+            if (eye.getId() != id) {
+                eye.setMode(GooglyEyeWidget.Mode.PLACED);
+            }
+        }
+    }
 }
